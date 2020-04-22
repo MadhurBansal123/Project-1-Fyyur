6638aabe85dff69dcfbebe17adc097001f974c25
MadhurBansal1997 <madhur.bansal@cleartrip.com>
2020-04-22 09:07:02 +0530

    First Commit

---
+++ model.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# connect to a local postgresql database
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+Show = db.Table('Show', db.Model.metadata, 
+    db.Column('Venue_id', db.Integer, db.ForeignKey('Venue.id')),
+    db.Column('Artist_id', db.Integer, db.ForeignKey('Artist.id')),
+    db.Column('start_time', db.DateTime)
+)
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String()))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    venues = db.relationship('Artist', secondary=Show, backref=db.backref('shows', lazy='joined'))
+    
+    def __repr__(self):
+        return f'<Venue Id: {self.id}, name: {self.name}>'
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    genres = db.Column(db.ARRAY(db.String()))
+    
+    def __repr__(self):
+        return f'<Artist Id: {self.id}, name: {self.name}>'
